84d2a88025c88651a6ad7fa7960a6db8b094698c
Aayush Patni <aayupatni@gmail.com>
2022-01-14 14:13:23 +0000

    Add starter code for exercise

---
+++ LambdaExercise/src/App.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        List<Person> people = Arrays.asList(
+                new Person("Charles", "Dickens", 60),
+                new Person("Lewis", "Carrol", 42),
+                new Person("Thomas", "Carlyle", 51),
+                new Person("Charlotte", "Bronte", 45),
+                new Person("Matthew", "Arnold", 38)
+        ); 
+
+        //Step-1: Sort list by lastName
+        
+        //Step-2: Create a method that prints all the elements in the list
+
+        //Step-3: Create a method that prints all people having lastName beginning with a C
+    }
+}
